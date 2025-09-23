f5b9c86a84e2a8e515b272e12cbdf33e901410aa
yassinesadri45@gmail.com <yassinesadri45@gmail.com>
2025-09-23 11:41:46 +0100

    challengee1

---
+++ Day1/variables/challenge2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int temperature;
+    int k;
+    k=temperature+1093.61;
+    printf("entre votre valeure temperature:  ");
+    scanf("%d",&temperature);
+    printf("le transforme en kelevenest : %d ",k);
+
+    return 0;
+}
