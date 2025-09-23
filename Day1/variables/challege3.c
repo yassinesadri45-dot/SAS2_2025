69813a5f78c28e6313d415f9f3e0c4ffb911db3a
yassinesadri45@gmail.com <yassinesadri45@gmail.com>
2025-09-23 12:30:21 +0100

    challege3.c

---
+++ Day1/variables/challege3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int km;
+    int yards;
+    yards= km*1093.61;
+    printf("entre la distance km:  ");
+    scanf("%d",&km);
+    printf(" la transform en yards:  ",yards);
+
+    return 0;
+}
+
+
+
