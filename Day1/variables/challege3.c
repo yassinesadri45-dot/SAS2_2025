35670efcc66d263433dde1ba2f364005c7ba2d15
yassinesadri45@gmail.com <yassinesadri45@gmail.com>
2025-09-23 14:12:48 +0100

    challenge4.c

--- Day1/variables/challege3.c
+++ Day1/variables/challege3.c
@@ -4,12 +4,12 @@
 int main(){
     int km;
     float yards;
-   
+
     printf("entre la distance km:  \n");
     scanf("%d",&km);
-    
+
      yards = km*1093.61;
-     
+
     printf(" la transform en yards  %.2f \n",yards);
 
     return 0;
---
+++ Day1/variables/challenge4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int km;
+    float ms;
+    printf("etre vetasse en kilometre:  ",km);
+    scanf("%d",&km);
+    ms= km*0.27778;
+        printf("transfore en metre par scond:%f  ",ms);
+
+    return 0;
+}
